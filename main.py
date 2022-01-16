a8085c87c029e648aba9b65c0514a747a5193035
AarushiKumar1 <>
2022-01-16 18:41:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Class Question
+# properties: The question, The Options, The Answer
+
+class Question:
+  def __init__(self, question, options, answer):
+    self.question = question
+    self.options = options
+    self.answer = answer
+
+q1 = Question("how old am I?", [13,17,57,12], 12)
+
+# Quiz class
+# properties: Questions, Score
+# methods: Answer, Ask
