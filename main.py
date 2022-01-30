3b841921a1b4b2e13f5f557957fa21bea828b47d
ak <97849487+aark-03@users.noreply.github.com>
2022-01-30 17:34:58 +0000

    added ask and options

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ class Question:
     self.question = question
     self.options = options
     self.answer = answer
-  def giveanswer():
-    print(1)
+  def giveanswer(self):
+    print(self.answer)
+  def ask(self):
+    print(self.question)
+    for opt in self.options:
+      print(opt)
 
 q1 = Question("how old am I?", [13,17,57,12], 12)
+q1.ask()
 
 # Quiz class
 # properties: Questions, Score
